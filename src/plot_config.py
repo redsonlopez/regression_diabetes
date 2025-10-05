056b512db431c2013e4b89ec45b2697e06580f12
redsonlopez <redsonlopez@gmail.com>
2025-10-04 22:11:19 -0300

    update

--- src/plot_config.py
+++ src/plot_config.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Configurações de estilo para os gráficos
 plot_config= {
         "axes.facecolor": "#111",     # Cor de fundo dos gráficos
         "figure.facecolor": "#111",   # Cor de fundo da figura
@@ -16,13 +15,10 @@ plot_config= {
         "grid.color": "#222",         # Cor da grade
         "grid.linestyle": "--",       # Estilo da grade
         "figure.figsize": (15, 6)     # Tamanho padrão da figura
-    }
+}
 
-# Função para o Matplotlib
 def set_matplotlib():
-    # plt.style.use(plot_config)
     plt.rcParams.update(plot_config)
 
-# Função para o Seaborn
 def set_seaborn():
     sns.set_theme(rc= plot_config)
